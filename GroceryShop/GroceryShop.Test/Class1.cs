2cb302c588712f5431fcdef383a12085baf3fcc4
Alfredo Andino Ruiz <aandino@abarcahealth.com>
2020-05-28 12:38:19 -0400

    configure unit test project include ThisShouldPass test.

---
+++ GroceryShop/GroceryShop.Test/Class1.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using Xunit;
+
+namespace GroceryShop.Test
+{
+    public class Class1
+    {
+        [Fact]
+        public void ThisShouldPass()
+        {
+            var expected = 1;
+            var actual = 1;
+
+            Assert.Equal(expected, actual);
+        }
+    }
+}
